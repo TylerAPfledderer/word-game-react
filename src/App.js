a48b0a731c52d18586f7b362b213882ed9343eca
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2020-11-13 16:44:49 -0500

    "Complete initial build of this refactored project in components prior to adding state changes.  Add initial data within each component before moving to App.js."

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import StartOverlay from './components/StartOverlay';
+import Phrase from './components/Phrase';
+import Keyboard from './components/Keyboard';
+import Scoreboard from './components/Scoreboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <StartOverlay />
+      <div id="banner" className="section">
+        <h2 className="header">Wheel of Success</h2>
+      </div>
+      <Phrase />
+      <Keyboard />
+      <Scoreboard />
     </div>
   );
 }
